7fa9ca8f65bf57c009da3c26605d2ee344c220ac
GMAX-PC\GMax <maxv@posteo.de>
2017-06-13 11:13:10 +0200

    Addded Log Macro in Tank Controller

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -1,9 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-#include "Tank.h"
+#include "Public/Tank.h"
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+	}	
+};
 
 const ATank *ATankPlayerController::GetControlledTank() 
 {
