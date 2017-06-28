db988fae87fb598c31d82c2cdbd90845032d5577
GMAX-PC\GMax <maxv@posteo.de>
2017-06-28 20:27:22 +0200

    Refined movements and input bindings

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,12 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
-
+void UTankMovementComponent::Initialise(UTankTrack *LeftTrackToSet, UTankTrack *RightTrackToSet)
+{
+	if (!LeftTrack || !RightTrack)
+	{
+		LeftTrack = LeftTrackToSet;
+		RightTrack = RightTrackToSet;
+	}
+}
 
 
 void UTankMovementComponent::IntentMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intent move forward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+	// TODO prevent double speed due to seperate controls
+}
+
+void UTankMovementComponent::IntentTurnRight(float Throw)
+{
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+
+	// TODO prevent double speed due to seperate controls
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,10 +6,12 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
  * 
  */
-UCLASS()
+UCLASS( meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
@@ -18,6 +20,15 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntentMoveForward(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void Initialise(UTankTrack *LeftTrackToSet, UTankTrack *RightTrackToSet);
+
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntentTurnRight(float Throw);
 	
-	
+private:
+	UTankTrack *LeftTrack = nullptr;
+	UTankTrack *RightTrack = nullptr;
 };
+	
